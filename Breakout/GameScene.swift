e6d333b9ef4eeb296d01605cb9b396d754652412
Jack Kamaryt <jkamaryt46@gmail.com>
2023-03-07 14:24:34 -0600

    Created Brick object (I know its out of order) * accidentally skipped over it

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -12,6 +12,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
+    var brick = SKSpriteNode()
     var loseZone = SKSpriteNode()
     
     override func didMove(to view: SKView) {
@@ -27,6 +28,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         // this stuff happens before each game starts
         makeBall()
         makePaddle()
+        makeBrick()
     }
     
     func kickBall() {
@@ -82,6 +84,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
         paddle.physicsBody?.isDynamic = false
         addChild(paddle)
+    }
+    
+    func makeBrick() {
+        brick.removeFromParent()    // remove the brick, if it exists
+        brick = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 20))
+        brick.position = CGPoint(x: frame.midX, y: frame.maxY - 50)
+        brick.name = "brick"
+        brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+        brick.physicsBody?.isDynamic = false
+        addChild(brick)
     }
     
     func makeLoseZone() {
