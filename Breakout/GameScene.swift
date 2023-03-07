82c42ae6c7526a98e7714e958be92de635b99a93
Jack Kamaryt <jkamaryt46@gmail.com>
2023-03-07 14:29:54 -0600

    Determine winners and losers

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -119,4 +119,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+    func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.node?.name == "brick" ||
+            contact.bodyB.node?.name == "brick" {
+            print("You Win!")
+            brick.removeFromParent()
+            ball.removeFromParent()
+        }
+        if contact.bodyA.node?.name == "loseZone" ||
+            contact.bodyB.node?.name == "loseZone" {
+            print("You Lose!")
+            ball.removeFromParent()
+        }
+    }
+    
+    
 }
